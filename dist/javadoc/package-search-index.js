c1e7c19e54e64949fca3e3227cf4c342acb9ff70
ddbaque <baquedavid0@gmail.com>
2024-11-21 16:50:36 +0100

    git: ignore files

--- dist/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"edu.epsevg.prop.lab.c4"}]
--- dist/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"edu.epsevg.prop.lab.c4","l":"Aleatori"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"edu.epsevg.prop.lab.c4","l":"ConEcta"},{"p":"edu.epsevg.prop.lab.c4","l":"Juga2"},{"p":"edu.epsevg.prop.lab.c4","l":"Manual"},{"p":"edu.epsevg.prop.lab.c4","l":"NewJFrame"},{"p":"edu.epsevg.prop.lab.c4","l":"Prova"}]
